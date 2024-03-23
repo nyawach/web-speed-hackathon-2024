063c714a7c3f399b869e04dae58c38527eb0578f
nyawach <yuunyawach@gmail.com>
2024-03-24 03:57:56 +0900

    useEpisode の fallbackData 入れ込む

--- workspaces/app/src/features/episode/hooks/useEpisode.ts
+++ workspaces/app/src/features/episode/hooks/useEpisode.ts
@@ -1,7 +1,36 @@
-import useSWR from 'swr';
+import useSWR from 'swr'; // Import SWRResponse
 
 import { episodeApiClient } from '../apiClient/episodeApiClient';
 
 export function useEpisode(...[options]: Parameters<typeof episodeApiClient.fetch>) {
-  return useSWR(episodeApiClient.fetch$$key(options), episodeApiClient.fetch, { ssr: false, suspense: true });
+  return useSWR(
+    episodeApiClient.fetch$$key(options),
+    episodeApiClient.fetch,
+    {
+      fallbackData: {
+        book: {
+          author: {
+            description: '',
+            id: '',
+            image: { alt: '', id: '' },
+            name: '',
+            nameRuby: '',
+          },
+          description: '',
+          id: '',
+          image: { alt: '', id: '' },
+          name: '',
+          nameRuby: '',
+        },
+        chapter: 0,
+        description: '',
+        id: '',
+        image: { alt: '', id: '' },
+        name: '',
+        nameRuby: '',
+        pages: [],
+      },
+      suspense: true,
+    }
+  )
 }
