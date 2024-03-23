f2bb71e51229a9c6c9d2f08b15bf8f790cddffd2
nyawach <yuunyawach@gmail.com>
2024-03-23 15:17:51 +0900

    sourcemap: trueにする

--- workspaces/client/tsup.config.ts
+++ workspaces/client/tsup.config.ts
@@ -58,7 +58,7 @@ export default defineConfig(async (): Promise<Options[]> => {
       outDir: OUTPUT_DIR,
       platform: 'browser',
       shims: true,
-      sourcemap: 'inline',
+      sourcemap: true,
       splitting: true,
       target: ['chrome58', 'firefox57', 'safari11', 'edge18'],
       treeshake: true,
