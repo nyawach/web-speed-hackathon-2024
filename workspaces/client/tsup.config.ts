a1e10e782a09136242c173a53a34400fbc2352f2
nyawach <yuunyawach@gmail.com>
2024-03-23 14:20:42 +0900

    splittingとtreeshakeをtrueに

--- workspaces/client/tsup.config.ts
+++ workspaces/client/tsup.config.ts
@@ -59,9 +59,9 @@ export default defineConfig(async (): Promise<Options[]> => {
       platform: 'browser',
       shims: true,
       sourcemap: 'inline',
-      splitting: false,
+      splitting: true,
       target: ['chrome58', 'firefox57', 'safari11', 'edge18'],
-      treeshake: false,
+      treeshake: true,
     },
   ];
 });
