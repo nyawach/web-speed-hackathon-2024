bd908e4fbeb436bd1528b8c39877594dcbc73d05
nyawach <yuunyawach@gmail.com>
2024-03-24 18:53:29 +0900

    magika消す

--- workspaces/admin/src/lib/image/isSupportedImage.ts
+++ workspaces/admin/src/lib/image/isSupportedImage.ts
@@ -1,28 +1,14 @@
 import { fileTypeFromBuffer } from 'file-type';
-import { Magika } from 'magika';
 
-const SUPPORTED_MAGIKA_LABEL_LIST = ['bmp', 'jpeg', 'png', 'webp'];
+const SUPPORTED_FILE_TYPE_LABEL_LIST = ['bmp', 'jpeg', 'png', 'webp'];
 const SUPPORTED_MIME_TYPE_LIST = ['image/bmp', 'image/jpeg', 'image/png', 'image/webp', 'image/avif', 'image/jxl'];
 
-const magika = new Magika();
-
-const initMagikaPromise = magika.load({
-  configURL: '/assets/magika/config.json',
-  modelURL: '/assets/magika/model.json',
-});
-
 export async function isSupportedImage(image: File): Promise<boolean> {
-  await initMagikaPromise;
-  const prediction = await magika.identifyBytes(new Uint8Array(await image.arrayBuffer()));
-
-  if (SUPPORTED_MAGIKA_LABEL_LIST.includes(prediction.label)) {
-    return true;
-  }
-
   const fileType = await fileTypeFromBuffer(await image.arrayBuffer());
-  if (SUPPORTED_MIME_TYPE_LIST.includes(fileType?.mime ?? '')) {
-    return true;
+  if (fileType) {
+    return SUPPORTED_FILE_TYPE_LABEL_LIST.includes(fileType.ext);
   }
 
-  return false;
+  const mimeType = image.type;
+  return SUPPORTED_MIME_TYPE_LIST.includes(mimeType);
 }
