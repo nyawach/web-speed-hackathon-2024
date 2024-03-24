d52434155e98bc1ecb2f9019b4adef6dafcba749
nyawach <yuunyawach@gmail.com>
2024-03-24 14:06:16 +0900

    description 消せてなかった

--- workspaces/schema/src/api/features/GetFeatureListResponse.ts
+++ workspaces/schema/src/api/features/GetFeatureListResponse.ts
@@ -17,7 +17,6 @@ export const GetFeatureListResponseSchema = createSelectSchema(feature)
       .extend({
         author: createSelectSchema(author)
           .pick({
-            description: true,
             id: true,
             name: true,
           })
